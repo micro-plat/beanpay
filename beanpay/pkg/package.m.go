bc76084be41ba8bfcaf5e757c47d2b493f0c5e18
zkfy <lib4net@163.com>
2019-07-31 15:37:40 +0800

    修改pkg

---
+++ beanpay/pkg/package.m.go
@@ -0,0 +1,15 @@
+package pkg
+
+type PKG struct {
+	ID             int64  `json:"pkg_id" m2s:"pkg_id"`
+	AccountID      int    `json:"account_id" m2s:"account_id"`
+	SPKGID         string `json:"spkg_id" m2s:"spkg_id"`
+	Name           string `json:"pkg_name" m2s:"pkg_name"`
+	TotalCapacity  int    `json:"total_capacity" m2s:"total_capacity"`
+	RemainCapacity int    `json:"total_remain" m2s:"total_remain"`
+	DailyCapacity  int    `json:"capacity_daily" m2s:"capacity_daily"`
+	TodayCapacity  int    `json:"deduct_today" m2s:"deduct_today"`
+	Expires        string `json:"expires" m2s:"expires"`
+	BookTime       string `json:"book_time" m2s:"book_time"`
+	LastUpdate     string `json:"last_update" m2s:"last_update"`
+}
