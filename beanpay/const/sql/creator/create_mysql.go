2debfde4a57a5c8173ed7213ae0673e2935b2ad1
zkfy <lib4net@163.com>
2019-10-24 10:00:57 +0800

    修改数据库引用

---
+++ beanpay/const/sql/creator/create_mysql.go
@@ -0,0 +1,12 @@
+// +build !oracle
+
+package creator
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/micro-plat/lib4go/db"
+)
+
+func CreateDB(xdb db.IDB) error {
+	return db.CreateDB(xdb, "src/github.com/micro-plat/beanpay/beanpay/const/sql/mysql")
+}
---
+++ beanpay/const/sql/creator/create_oracle.go
@@ -0,0 +1,12 @@
+// +build oracle
+
+package creator
+
+import (
+	"github.com/micro-plat/lib4go/db"
+	_ "github.com/zkfy/go-oci8"
+)
+
+func CreateDB(xdb db.IDB) error {
+	return db.CreateDB(xdb, "src/github.com/micro-plat/beanpay/beanpay/const/sql/oracle")
+}
