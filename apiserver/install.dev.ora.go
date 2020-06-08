155d8582dbd9f084d638f32d49aa2c8344677545
luyue <luyue@100bm.cn>
2020-06-08 15:45:43 +0800

    测试

--- apiserver/install.dev.ora.go
+++ apiserver/install.dev.ora.go
@@ -11,5 +11,5 @@ import (
 //bindConf 绑定启动配置， 启动时检查注册中心配置是否存在，不存在则引导用户输入配置参数并自动创建到注册中心
 func init() {
 	app.IsDebug = true
-	app.Conf.Plat.SetDB(conf.NewOracleConf("oms", "123456", "orcl136"))
+	app.Conf.Plat.SetDB(conf.NewOracleConf("hydra", "123456", "orcl136"))
 }
